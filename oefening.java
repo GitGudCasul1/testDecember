ed127e28aacee8817410f3428b44162cdd930787
Jef Den Hond <jdenhond@ION.local>
2019-12-10 09:51:22 +0100

    method

--- oefening.java
+++ oefening.java
@@ -1,5 +1,9 @@
 public class ErrorClass {
-    public static void main(String[] args) {
-        System.out.println("Hello world!")
-    } 
+    	public static void main(String[] args) {
+        	PrintHelloWorld();
+    	} 
+
+	public static void PrintHelloWorld(){
+		System.out.println("Hello world!");
+	}
 }
